92fc6b0dfb24c875cc8e00a2dc6161a3963ba915
Pi Home Server <pihomeserver@gmail.com>
2016-10-25 21:25:53 +0200

    Update User model to include password reset token

--- database/models/User.js
+++ database/models/User.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const crypto = require('crypto');
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
       username: {
@@ -11,7 +13,9 @@ module.exports = function(sequelize, DataTypes) {
           notEmpty: true
         }
       },      
-      password      : DataTypes.STRING,
+      password            : DataTypes.STRING,
+      passwordResetToken  : DataTypes.STRING,
+      passwordResetExpires: DataTypes.DATE,
       email: {
         type: DataTypes.STRING,
         unique: {
@@ -26,7 +30,11 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     instanceMethods: {
       gravatar: function (size = 200) {
-        return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+        if (!this.email) {
+          return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+        }
+        var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+        return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
       }
     }
   });
